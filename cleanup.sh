65a7d2396b4b6becf10789b28cd7d828dc503d68
Laurent Franceschetti <laurent.franceschetti@settlenext.com>
2024-10-14 07:50:18 +0200

    Bump version number and add script file
    	Add new script file `cleanup.sh`

---
+++ cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This may be useful to get a clean slate (for creating a distribution)
+
+# Find and delete all __pycache__ directories
+find . -type d -name "__pycache__" -exec rm -rf {} +
+
+# Find and delete all directories starting with __
+find . -type d -name "__*" -exec rm -rf {} +
+
+# Find and delete all .egg-info directories
+find . -type d -name "*.egg-info" -exec rm -rf {} +
+
+# Find and delete all .egg files
+find . -type f -name "*.egg" -exec rm -f {} +
+
+echo "Cleanup complete!"
